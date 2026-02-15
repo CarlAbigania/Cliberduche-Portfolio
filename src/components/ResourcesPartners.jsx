7d012f689ae881c95e3cdb23377728a46ce89d46
John Carlo Abigania <abiganiacarlo687@gmail.com>
2026-02-15 21:23:48 +0800

    Tablet - 2 columns in resource and partner section for it to not compressed looking

--- src/components/ResourcesPartners.jsx
+++ src/components/ResourcesPartners.jsx
@@ -99,7 +99,7 @@ const ResourcesPartners = () => {
         </div>
 
         {/* Stats */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
           {[
             {
               number: '20M+',
